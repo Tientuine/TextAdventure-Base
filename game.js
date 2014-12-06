45bade8b9e09a8e1eca87bb4b4abd8049204409c
Matthew A Johnson <j.matthew.a@gmail.com>
2014-12-06 10:52:58 -0500

    Updated to provide more support to students.
    I decided to add some more suggested functions and related comments in the hope that these will help all students to complete their games.

--- game.js
+++ game.js
@@ -1,47 +1,97 @@
-// Global player object (later this will no longer be global)
-var player = {
-	items: [],
-	// add a property for the player's current location
-};
+/*
+NOTES:
+    This file contains the overall game logic - including
+functions that display output, drive the game loop, etc.
+    You are not required to have all of these functions, nor
+any of them - that is up to you. However, you are permitted to
+use any of this code in your own solution, with whatever changes
+and additions you need to make.
+*/
 
-player.goto = function(loc) {
-	// check if loc is valid (exists in the map)
-		// if so, then check if loc is connected to player's current location
-			// if so, then do any other logic you need (like checking for prerequisite items)
-			// and finally set player's current location to loc
-		// if not...
-			// display feedback to the player that they can't reach loc from here
-	// if not...
-		// display feedback to the player that loc does not exist
+/*
+    Before starting the game proper, we might ask the user to enter some info.
+    Note that this takes advantage of adding and removing event listeners.
+    If we want the player to go through more than one intro screen, then
+    instead of calling gameStart() we could call the next intro function.
+    We can set up a series of these functions, and sort of bounce from one
+    to the next through clever use of the listeners.
+*/
+function gameIntro() {
+	var inputBox = document.querySelector("input");
+	var listener = function(event){
+		if (event.keyCode === 13) {
+		    // remove this listener before continuing so it only runs once
+		    event.target.removeEventListener(listener);
+			customizePlayer(this.value);
+			gameStart();
+		}
+	};
+	inputBox.addEventListener("keyup", listener);
 }
 
-player.pickup = function(item){
-	// check if item is present in current location
-		// if so, remove it from the current location
-		// and add it to player's items
-		this.items.push(item);
-	// if not...
-		// inform the user that item is not here
-};
-
-player.drop = function(item){
-	var pos = this.items.indexOf(item);
-	if (pos >= 0) {
-		// remove it from player's items
-		this.items.splice(pos, 1);
-		// and add it to the current location
-	}
-};
-
-// example of recognizing the need for a helper function and then writing it!
-// we could set innerHTML to empty string - but this is better!
-function clearContent(node) {
-    while (node.hasChildNodes()) {
-        node.removeChild(node.firstChild);
-    }
+/*
+    Start the main game loop.
+*/
+function gameStart() {
+	var inputBox = document.querySelector("input");
+	inputBox.addEventListener("keyup", function(event){
+		if (event.keyCode === 13) {
+			gameStep(this.value);
+		}
+	});
 }
 
-// loop over each player method and add it to the Web page
+/*
+    Run one pass of the game loop.
+*/
+function gameStep (input) {
+	var cmd = interpret(input); // parse the user input
+	var result = execute(cmd); // run the desired command
+	report(result); // display the results on the screen
+}
+
+/*
+    Parse the user's input and use it to customize certain player properties.
+*/
+function customizePlayer(input) {
+    // here we should set the player's name and/or other properties
+}
+
+/*
+    Parse and normalize the user input string.
+*/
+function interpret (input) {
+    console.log("calling interpret"); // remove when finished debugging
+
+    var cmd = {}, tokens = input.trim().toLowerCase().split(" ");
+    cmd.action = tokens.shift();
+    cmd.target = tokens.join(" ");
+	return cmd;
+}
+
+/*
+    Perform the desired player action.
+*/
+function execute (command) {
+	console.log("calling execute"); // remove when finished debugging
+
+	player[command.action](command.target);
+}
+
+/*
+    Display any results/changes on the page.
+*/
+function report (result) { // note: parameter not currently used
+	console.log('calling report'); // remove when finished debugging
+	
+	displayActions();
+	displayInventory();
+	displayScene();
+}
+
+/*
+    Loop over each player method and add it to the Web page.
+*/
 function displayActions() {
     var field, action, actionList;
 
@@ -57,7 +107,9 @@ function displayActions() {
     }
 }
 
-// loop over each inventory item and add it to the Web page
+/*
+    Loop over each inventory item and add it to the Web page.
+*/
 function displayInventory() {
     var i, item, inventory;
 
@@ -71,51 +123,21 @@ function displayInventory() {
     }
 }
 
+/*
+    Get the description of the player's current location and write it to the page.
+*/
 function displayScene() {
     // Hmmm... need to implement this function...
 }
 
-// parse and normalize the user input string
-function interpret (input) {
-    console.log("calling interpret"); // remove when finished debugging
-
-    var cmd = {}, tokens = input.trim().toLowerCase().split(" ");
-    cmd.action = tokens.shift();
-    cmd.target = tokens.join(" ");
-	return cmd;
-}
-
-// perform the desired player action
-function execute (command) {
-	console.log("calling execute"); // remove when finished debugging
-
-	player[command.action](command.target);
-}
-
-// display any results/changes on the page
-function report (result) { // note: parameter not currently used
-	console.log('calling report'); // remove when finished debugging
-	
-	displayActions();
-	displayInventory();
-	displayScene();
-}
-
-// run one pass of the game loop
-function gameStep (input) {
-	var cmd = interpret(input); // parse the user input
-	var result = execute(cmd); // run the desired command
-	report(result); // display the results on the screen
-}
-
-var gameStart = function() {
-	var inputBox = document.querySelector("input");
-	inputBox.addEventListener("keyup", function(event){
-		if (event.keyCode === 13) {
-			gameStep(this.value);
-		}
-	});
+/*
+    This is an example of recognizing the need for a helper function and then writing it!
+    We could just set innerHTML to empty string - but this is better (more efficient)!
+*/
+function clearContent(node) {
+    while (node.hasChildNodes()) {
+        node.removeChild(node.firstChild);
+    }
 }
 
 window.onload = gameStart; // game starts only after the page is loaded
-
