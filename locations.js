9022f3dc6810630794aef8585a670d1291621583
Matthew A Johnson <j.matthew.a@gmail.com>
2014-12-06 11:05:56 -0500

    Added some additional guidance in this file.
    I hope that the additions here will provide helpful suggestions and guide all students toward a successful final game.

--- locations.js
+++ locations.js
@@ -1,10 +1,35 @@
-function Location(name, descrip) {
+/*
+NOTES:
+    All functions in this file are suggestions and use of the code below
+is permissible and even encouraged. However, feel free to add/change/remove
+anything you see here in your own games. And, if you decide you want to
+write your own code from scratch, you are not required to use my code at all.
+*/
+
+/*
+    Constructor used to create all the locations in the game.
+    Notice that I have added an initialItems parameter so that
+    we can start the location out with some items in it.
+    You might need to add more parameters to this constructor
+    if there are other properties that need initial values.
+*/
+function Location(name, descrip, initialItems) {
     this.name = name;
     this.description = function() {
         return descrip;
     }
-    this.items = [];
+    this.items = initialItems;
+    // add any additional properties that you need, suggested ideas include:
+    //  - a list of prerequisite items needed to enter the location
+    //  - boolean variable to indicate locked/unlocked
+    //  - boolean variable to indicate dark/illuminated
+    //  - list of monsters/enemies/etc. in the location
+    //  - anything else you can think of that work for your story!
 }
+
+// The Location functions below are a good start, buy you might well
+// decide you need other functions to support additional properties,
+// such as any prereqs, monsters, etc. that you add to your game.
 
 Location.prototype.has = function(item) {
     // this will be like player.has from earlier examples
@@ -17,6 +42,11 @@ Location.prototype.remove = function(item) {
 Location.prototype.put = function(item) {
     // this will be similar to the original version of player.pickup
 }
+
+/*
+    NOTE: For the functions below, we could instead make them methods of our
+    map object - then we could call them as map.connect(from,to)
+*/ 
 
 function indexOfLocation = function(map, locName) {
     // this should work like indexOf, but searches for a location whose name matches
@@ -40,9 +70,9 @@ function disconnect(map, from, to) {
 // sample map - like the one you created during lab
 var map = {
     locations: [
-        new Location("Beach", "You are on a warm, sandy beach."),
-        new Location("Cliffs", "Above you tower sheer cliffs 100 feet high."),
-        new Location("Shack", "Before you is a shack, seemingly abandoned.")
+        new Location("Beach", "You are on a warm, sandy beach.", ["fishing net"]),
+        new Location("Cliffs", "Above you tower sheer cliffs 100 feet high.", ["backpack", "handkerchief"]),
+        new Location("Shack", "Before you is a shack, seemingly abandoned.", ["rope"])
     ],
     connections: [ [0, 0, 0], [0, 0, 0], [0, 0, 0] ]
 };
@@ -52,6 +82,7 @@ connect(map, 0, 1);
 connect(map, 1, 2);
 connect(map, 2, 0);
 
+// little test function so we can get used to the matrix and see if it works
 function testMap() {
     var j;
     console.log("The " + map.locations[0].name + " is adjacent to:");
@@ -62,4 +93,4 @@ function testMap() {
     }
 }
 
-//testMap();
+//testMap(); // no need to have this anymore once we've tested it
